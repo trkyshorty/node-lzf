283a29d02ed0ba11e0f43332617d7a0ecb23b5ca
Türkay Tanrikulu <trky.shorty@gmail.com>
2025-06-05 09:31:05 +0300

    Update lzf.cc

--- src/lzf.cc
+++ src/lzf.cc
@@ -2,12 +2,6 @@
 /* node-lzf (C) 2025 Maintained Türkay Tanrikulu <trky.shorty@gmail.com>  */
 
 #include <node_buffer.h>
-#include <stdlib.h>
-
-#ifdef __APPLE__
-#include <malloc/malloc.h>
-#endif
-
 #include "nan.h"
 #include "lzf/lzf.h"
 
@@ -15,76 +9,47 @@ using namespace v8;
 using namespace node;
 
 NAN_METHOD(compress) {
-    if (info.Length() < 1 || !info[0]->IsObject() || !node::Buffer::HasInstance(info[0])) {
+    if (info.Length() < 1 || !info[0]->IsObject() || !Buffer::HasInstance(info[0])) {
         return Nan::ThrowTypeError("First argument must be a Buffer");
     }
 
-    Local<Object> inBuf = Nan::To<Object>(info[0]).ToLocalChecked();
-    char* dataIn = node::Buffer::Data(inBuf);
-    size_t lenIn = node::Buffer::Length(inBuf);
+    Local<Object> inputBuf = Nan::To<Object>(info[0]).ToLocalChecked();
+    char* inputData = Buffer::Data(inputBuf);
+    size_t inputLen = Buffer::Length(inputBuf);
 
-    size_t maxCompressedLen = lenIn + (lenIn / 16) + 64 + 3;
-    auto maybeBuf = Nan::NewBuffer(maxCompressedLen);
-    if (maybeBuf.IsEmpty()) {
-        return Nan::ThrowError("Failed to allocate output buffer");
-    }
+    size_t maxOutputLen = inputLen + (inputLen / 16) + 64 + 3;
+    std::unique_ptr<char[]> output(new char[maxOutputLen]);
 
-    Local<Object> outBuf = maybeBuf.ToLocalChecked();
-    char* outData = node::Buffer::Data(outBuf);
-
-    unsigned int compressedLen = lzf_compress(dataIn, lenIn, outData, maxCompressedLen);
+    unsigned int compressedLen = lzf_compress(inputData, inputLen, output.get(), maxOutputLen);
     if (compressedLen == 0) {
         return Nan::ThrowError("Compression failed");
     }
 
-    // Avoid .Get + .Call + new[]
-    Local<Function> sliceFn = outBuf->Get(Nan::GetCurrentContext(), Nan::New("slice").ToLocalChecked())
-        .ToLocalChecked().As<Function>();
-    Local<Value> args[] = {
-        Nan::New(0),
-        Nan::New((uint32_t)compressedLen)
-    };
-    Local<Value> sliced = sliceFn->Call(Nan::GetCurrentContext(), outBuf, 2, args).ToLocalChecked();
-
-    info.GetReturnValue().Set(sliced);
+    info.GetReturnValue().Set(Nan::CopyBuffer(output.get(), compressedLen).ToLocalChecked());
 }
 
 NAN_METHOD(decompress) {
-    if (info.Length() < 1 || !info[0]->IsObject() || !node::Buffer::HasInstance(info[0])) {
+    if (info.Length() < 1 || !info[0]->IsObject() || !Buffer::HasInstance(info[0])) {
         return Nan::ThrowTypeError("First argument must be a Buffer");
     }
 
-    Local<Object> inBuf = Nan::To<Object>(info[0]).ToLocalChecked();
-    char* dataIn = node::Buffer::Data(inBuf);
-    size_t lenIn = node::Buffer::Length(inBuf);
+    Local<Object> inputBuf = Nan::To<Object>(info[0]).ToLocalChecked();
+    char* inputData = Buffer::Data(inputBuf);
+    size_t inputLen = Buffer::Length(inputBuf);
 
-    size_t expectedOutLen = 999 * 1024 * 1024;
+    size_t expectedOutLen = 1024 * 1024 * 100;
     if (info.Length() > 1 && info[1]->IsNumber()) {
         expectedOutLen = Nan::To<uint32_t>(info[1]).FromJust();
     }
 
-    auto maybeBuf = Nan::NewBuffer(expectedOutLen);
-    if (maybeBuf.IsEmpty()) {
-        return Nan::ThrowError("Failed to allocate output buffer");
-    }
+    std::unique_ptr<char[]> output(new char[expectedOutLen]);
 
-    Local<Object> outBuf = maybeBuf.ToLocalChecked();
-    char* outData = node::Buffer::Data(outBuf);
-
-    unsigned int decompressedLen = lzf_decompress(dataIn, lenIn, outData, expectedOutLen);
+    unsigned int decompressedLen = lzf_decompress(inputData, inputLen, output.get(), expectedOutLen);
     if (decompressedLen == 0) {
         return Nan::ThrowError("Decompression failed");
     }
 
-    Local<Function> sliceFn = outBuf->Get(Nan::GetCurrentContext(), Nan::New("slice").ToLocalChecked())
-        .ToLocalChecked().As<Function>();
-    Local<Value> args[] = {
-        Nan::New(0),
-        Nan::New((uint32_t)decompressedLen)
-    };
-    Local<Value> sliced = sliceFn->Call(Nan::GetCurrentContext(), outBuf, 2, args).ToLocalChecked();
-
-    info.GetReturnValue().Set(sliced);
+    info.GetReturnValue().Set(Nan::CopyBuffer(output.get(), decompressedLen).ToLocalChecked());
 }
 
 NAN_MODULE_INIT(init) {
